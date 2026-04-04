dbacdb773305a04d6c57ea0e3021ad3ae421efd5
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2026-04-04 19:37:18 +0530

    fix: app.json syntax error

--- app.config.js
+++ app.config.js
@@ -3,13 +3,24 @@ const appJson = require('./app.json');
 export default {
     expo: {
         ...appJson.expo,
+
+        ios: {
+            ...appJson.expo?.ios,
+
+            bundleIdentifier: "com.avachatrushikesh.recurrly",
+
+            // ✅ MOVE THIS HERE
+            infoPlist: {
+                ITSAppUsesNonExemptEncryption: false,
+            },
+        },
+
         extra: {
             ...(appJson.expo?.extra || {}),
 
             posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
             posthogHost: process.env.POSTHOG_HOST,
 
-            // ✅ FIX: move inside extra.eas
             eas: {
                 projectId: "41df48ae-a7ac-4541-9184-e07ef8a7ea0a",
             },
