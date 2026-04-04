c6fc73864eed18ece778d7ec8a45eddcda33156b
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2026-04-04 19:03:20 +0530

    fix: sync package-lock.json

--- app.config.js
+++ app.config.js
@@ -1,12 +1,18 @@
-const appJson = require('./app.json')
+const appJson = require('./app.json');
 
 export default {
-  expo: {
-    ...appJson.expo,
-    extra: {
-      ...(appJson.expo?.extra || {}),
-      posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
-      posthogHost: process.env.POSTHOG_HOST,
+    expo: {
+        ...appJson.expo,
+        extra: {
+            ...(appJson.expo?.extra || {}),
+
+            posthogProjectToken: process.env.POSTHOG_PROJECT_TOKEN,
+            posthogHost: process.env.POSTHOG_HOST,
+
+            // ✅ FIX: move inside extra.eas
+            eas: {
+                projectId: "41df48ae-a7ac-4541-9184-e07ef8a7ea0a",
+            },
+        },
     },
-  },
-}
+};
